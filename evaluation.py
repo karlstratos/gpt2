cfa91644b799aefc5788028d064436c75d53f7ee
Karl Stratos <karlstratos@gmail.com>
2022-08-10 15:07:32 +0900

    .

--- evaluation.py
+++ evaluation.py
@@ -15,6 +15,9 @@ def compute_perplexity(model, loader, device):
             loss_sum += output.loss.item() * num_preds
             num_preds_total += num_preds
 
-    # TODO: Should we divide by the # tokens sep by whitespace to be comparable?
+    # Perplexity not completely compatible with works using different vocabs and
+    # segmentation schemes, but this seems to be what people use when they
+    # report GPT results:
+    # https://github.com/huggingface/transformers/blob/ed70f24291f110ebefb72a15582e04f2f2958280/examples/pytorch/language-modeling/run_clm_no_trainer.py#L613
     perp = math.exp(loss_sum / num_preds_total)
     return perp, loss_sum, num_preds_total
